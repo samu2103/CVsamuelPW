67337842500544f152cb4113e6970d0b2e4a8e8c
samue <samuelalbertoesc123@gmail.com>
2023-02-02 23:18:22 -0600

    [FIX] movimos un elemento en titulo

--- src/components/atoms/Titulo.jsx
+++ src/components/atoms/Titulo.jsx
@@ -3,7 +3,7 @@ function Titulo() {
     return ( 
       <>
         <h1 className="texH1">Samuel Escobar</h1>
-        <h3 className="texH2">Estudiante de ing. en desarrollo de software</h3>
+        <h3 className="texH2">Estudiante de ing: en desarrollo de software</h3>
       </>
      );
 }
